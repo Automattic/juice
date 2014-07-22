9dde997b1ed2bf28fe45a66b18c2ff0b9f71395c
Andrew Kelley <superjoe30@gmail.com>
2014-07-22 13:39:32 -0700

    update dependencies and code cleanup

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-/*globals describe:false it:false*/
-var juice = require('../')
-  , path = require('path')
-  , fs = require('fs')
-  , Batch = require('batch')
-  , assert = require('assert')
+var juice = require('../');
+var path = require('path');
+var fs = require('fs');
+var Pend = require('pend');
+var assert = require('assert');
+
+var it = global.it;
 
 var tests = [
   "doctype",
@@ -13,23 +14,31 @@ var tests = [
   "spaces_in_path",
 ];
 
+
 tests.forEach(function(testName) {
   it(testName, createIt(testName));
 });
 
 function createIt(testName) {
   return function(cb) {
-    var batch = new Batch();
-    batch.push(function(cb) {
+    var pend = new Pend();
+    var expected, actual;
+    pend.go(function(cb) {
       var filePath = path.join(__dirname, "html", testName + ".in.html");
-      juice(filePath, cb);
+      juice(filePath, function(err, val) {
+        actual = val;
+        cb(err);
+      });
     });
-    batch.push(function(cb) {
-      fs.readFile(path.join(__dirname, "html", testName + ".out.html"), 'utf8', cb);
+    pend.go(function(cb) {
+      fs.readFile(path.join(__dirname, "html", testName + ".out.html"), 'utf8', function(err, val) {
+        expected = val;
+        cb(err);
+      });
     });
-    batch.end(function(err, results) {
+    pend.wait(function(err) {
       if (err) return cb(err);
-      assert.strictEqual(results[1].trim(), results[0].trim());
+      assert.strictEqual(actual.trim(), expected.trim());
       cb();
     });
   };
