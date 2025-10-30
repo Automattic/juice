130184e02f4359c0fdaaade022a5911a9b738990
Cosmin Popovici <hellocosmin@gmail.com>
2025-10-30 14:07:53 +0200

    feat: remove inlined selectors

--- test/juice.test.js
+++ test/juice.test.js
@@ -254,3 +254,115 @@ it('`inlineDuplicateProperties` option', function() {
     '<div class="header" style="background-color: #000; background-color: rgba(0,0,0,0.8);"></div>'
   );
 });
+
+it('`removeInlinedSelectors` option', function() {
+  // Basic test - inlined selectors should be removed, media queries preserved
+  var result = juice(
+    '<style>div { color: red; } .test { background: blue; } @media (max-width: 600px) { div { color: green; } }</style><div class="test">Hello</div>',
+    { removeStyleTags: false, removeInlinedSelectors: true }
+  );
+  assert.ok(result.indexOf('style="color: red; background: blue;"') > -1, 'styles should be inlined');
+  assert.ok(result.indexOf('<style>') > -1, 'style tag should be preserved');
+  assert.ok(result.indexOf('@media') > -1, 'media query should be preserved');
+  assert.ok(result.indexOf('div {') === -1 || result.indexOf('@media') < result.indexOf('div {'), 'standalone div rule should be removed');
+  assert.ok(result.indexOf('.test {') === -1, '.test rule should be removed');
+
+  // Multiple selectors - all inlined selectors should be removed
+  result = juice(
+    '<style>div { color: red; } p { color: blue; } span { color: green; }</style><div>A</div><p>B</p>',
+    { removeStyleTags: false, removeInlinedSelectors: true }
+  );
+  assert.ok(result.indexOf('style="color: red;"') > -1, 'div styles should be inlined');
+  assert.ok(result.indexOf('style="color: blue;"') > -1, 'p styles should be inlined');
+  assert.ok(result.indexOf('span {') > -1, 'non-matched span rule should be preserved');
+
+  // Preserve @font-face
+  result = juice(
+    '<style>@font-face { font-family: "MyFont"; } div { color: red; }</style><div>Test</div>',
+    { removeStyleTags: false, removeInlinedSelectors: true, preserveFontFaces: true }
+  );
+  assert.ok(result.indexOf('@font-face') > -1, 'font-face should be preserved');
+  assert.ok(result.indexOf('div {') === -1, 'inlined div rule should be removed');
+
+  // Preserve keyframes
+  result = juice(
+    '<style>@keyframes slide { from { left: 0; } } div { color: red; }</style><div>Test</div>',
+    { removeStyleTags: false, removeInlinedSelectors: true, preserveKeyFrames: true }
+  );
+  assert.ok(result.indexOf('@keyframes') > -1, 'keyframes should be preserved');
+  assert.ok(result.indexOf('div {') === -1, 'inlined div rule should be removed');
+
+  // Preserve pseudos like :hover
+  result = juice(
+    '<style>div { color: red; } div:hover { color: blue; }</style><div>Test</div>',
+    { removeStyleTags: false, removeInlinedSelectors: true, preservePseudos: true }
+  );
+  assert.ok(result.indexOf('style="color: red;"') > -1, 'div styles should be inlined');
+  assert.ok(result.indexOf(':hover') > -1, 'hover pseudo should be preserved');
+
+  // When all rules are inlined, style tag should be removed
+  result = juice(
+    '<style>div { color: red; }</style><div>Test</div>',
+    { removeStyleTags: false, removeInlinedSelectors: true }
+  );
+  assert.ok(result.indexOf('<style>') === -1, 'empty style tag should be removed');
+
+  // Partial selector match - if selector has multiple comma-separated selectors and only some match
+  result = juice(
+    '<style>div, p, span { color: red; }</style><div>A</div>',
+    { removeStyleTags: false, removeInlinedSelectors: true }
+  );
+  assert.ok(result.indexOf('style="color: red;"') > -1, 'matched selector should be inlined');
+  // The remaining selectors (p, span) should still be in the style tag
+  assert.ok(result.indexOf('<style>') > -1, 'style tag should remain');
+  assert.ok(result.indexOf('p') > -1 || result.indexOf('span') > -1, 'unmatched selectors should remain');
+
+  // Works with extraCss option
+  result = juice(
+    '<div class="test">Hello</div>',
+    { 
+      extraCss: 'div { color: red; } .test { background: blue; } @media print { div { color: black; } }',
+      removeStyleTags: false,
+      removeInlinedSelectors: true
+    }
+  );
+  assert.ok(result.indexOf('style="color: red; background: blue;"') > -1, 'styles should be inlined');
+  
+  // removeStyleTags processes independently - removes non-preserved rules, keeps preserved ones
+  result = juice(
+    '<style>div { color: red; } @media print { div { color: black; } }</style><div>Test</div>',
+    { removeStyleTags: true, removeInlinedSelectors: true }
+  );
+  // removeInlinedSelectors shouldn't apply when removeStyleTags is true
+  assert.ok(result.indexOf('@media print') > -1, 'media query should be preserved with removeStyleTags');
+
+  // Email client targeting selectors
+  // Make sure we don't remove CSS selectors that aren't in the original HTML
+  result = juice(
+    '<style>div { color: red; } u + .body .gmail-fix { display: block; }</style><div>Test</div>',
+    { removeStyleTags: false, removeInlinedSelectors: true }
+  );
+  assert.ok(result.indexOf('style="color: red;"') > -1, 'div styles should be inlined');
+  assert.ok(result.indexOf('u + .body') > -1, 'Gmail targeting selector should be preserved (no match in HTML)');
+  assert.ok(result.indexOf('div {') === -1, 'inlined div rule should be removed');
+
+  // Multiple email client targeting selectors
+  result = juice(
+    '<style>p { margin: 0; } u + .body .gmail { color: red; } #outlook a { padding: 0; } .ExternalClass { width: 100%; }</style><p>Test</p>',
+    { removeStyleTags: false, removeInlinedSelectors: true }
+  );
+  assert.ok(result.indexOf('style="margin: 0;"') > -1, 'p styles should be inlined');
+  assert.ok(result.indexOf('u + .body') > -1, 'Gmail selector should be preserved');
+  assert.ok(result.indexOf('#outlook') > -1, 'Outlook selector should be preserved');
+  assert.ok(result.indexOf('.ExternalClass') > -1, 'Outlook.com selector should be preserved');
+  assert.ok(result.indexOf('p {') === -1, 'inlined p rule should be removed');
+
+  // Outlook.com (webmail) class prefix targeting
+  result = juice(
+    '<style>.header { background: blue; } [class~="x_header"] { max-width: 600px; }</style><div class="header">Test</div>',
+    { removeStyleTags: false, removeInlinedSelectors: true }
+  );
+  assert.ok(result.indexOf('style="background: blue;"') > -1, 'header styles should be inlined');
+  assert.ok(result.indexOf('[class~="x_header"]') > -1, 'Outlook.com x_ prefix selector should be preserved');
+  assert.ok(result.indexOf('.header {') === -1, 'inlined header rule should be removed');
+});
